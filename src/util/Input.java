12c7f28fbaf25f91f59151682e18d664bb31713d
Alexander Royal <alexander.de.royal@gmail.com>
2023-06-22 12:27:29 -0500

    person exercise completed

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,39 +1,53 @@
-package util;
-
-import java.util.Scanner;
-
-public class Input {
-    private Scanner scanner;
-
-    public Input(){
-        this.scanner = new Scanner(System.in);
-    }
-    public String getString(){
-        return this.scanner.nextLine();
-    }
-    public boolean yesNo() {
-        System.out.println("Yes or No?");
-
-        String userInput = this.scanner.nextLine();
-
-        userInput = userInput.toLowerCase();
-
-        switch (userInput) {
-            case ("no"):
-                return false;
-            case ("n"):
-                return false;
-            case ("yes"):
-                return true;
-            case ("maybe"):
-                System.out.println("Ain't you cute?");
-                return yesNo();
-            default:
-                System.out.println("Input is unacceptable");
-        }
-        public int getInt(){
-            System.out.println("Please enter an Int");
-            return this.scanner.nextInt();
-        }
-    }
-}
+//package util;
+//
+//import java.util.Scanner;
+//
+//public class Input {
+//    private Scanner scanner;
+//
+//    public Input(){
+//        this.scanner = new Scanner(System.in);
+//    }
+//    public String getString(){
+//        return this.scanner.nextLine();
+//    }
+//    public boolean yesNo() {
+//        System.out.println("Yes or No?");
+//
+//        String userInput = this.scanner.nextLine();
+//
+//        userInput = userInput.toLowerCase();
+//
+//        switch (userInput) {
+//            case ("no"):
+//                return false;
+//            case ("n"):
+//                return false;
+//            case ("yes"):
+//                return true;
+//            case ("maybe"):
+//                System.out.println("Ain't you cute?");
+//                return yesNo();
+//            default:
+//                System.out.println("Input is unacceptable");
+//        }
+//        public int getInt(){
+//            System.out.println("Please enter an Int");
+//            return this.scanner.nextInt();
+//        }
+//
+//        public int getInt(int min, int max){
+//            System.out.println("Please enter an a number within the range");
+//
+//            int userInput = this.scanner.nextInt();
+//            if(userInput > min || userInput < max){
+//                return userInput;
+//            } else {
+//                getInt(min, max);
+//            }
+//        }
+//    }
+////    public double getDouble(){
+//        System.out.println();
+//    }
+//}
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -7,6 +7,8 @@ public class InputTest {
         Input input = new Input();
 
         System.out.println(input.yesNo());
-        System.out.println();
+        System.out.println(input.getInt());
+        System.out.println(input.getDouble());
+        System.out.println("Input.getInt(5,10) = " + input.getInt(5,10));
     }
 }
